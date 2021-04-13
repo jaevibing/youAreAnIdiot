10ba8d6b50e01d8d1a1ec1f0003d8b6ae001ded1
jack-debug <55571956+jack-debug@users.noreply.github.com>
2021-04-13 20:15:14 +1000

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace youAreAnIdiot
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(647, 109);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "you are an idiot";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Wingdings", 90F);
+            this.label3.Location = new System.Drawing.Point(37, 147);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(598, 132);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "J J J";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.ClientSize = new System.Drawing.Size(675, 299);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Show;
+            this.Text = "you are an idiot";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿//////////////////////////////////////////
+///    jack-debug | You Are An Idiot   ///
+///    13/04/2021 | jack-debug.xyz     ///
+//////////////////////////////////////////
+
+using System;
+using System.Drawing;
+using System.IO;
+using System.Media;
+using System.Net;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace youAreAnIdiot
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void change()
+        {
+        change:
+            this.BackColor = Color.Black;
+            this.label1.ForeColor = Color.White;
+            this.label3.ForeColor = Color.White;
+            Thread.Sleep(500);
+            this.BackColor = Color.White;
+            this.label1.ForeColor = Color.Black;
+            this.label3.ForeColor = Color.Black;
+            Thread.Sleep(500);
+            goto change;
+        }
+        private void flyAround()
+        {
+        fly:
+            this.Left += 1;
+            this.Top += 1;
+            goto fly;
+        }
+        private void sound(string filee)
+        {
+            var soundPlayer = new SoundPlayer(filee);
+        a:
+            soundPlayer.PlaySync();
+            goto a;
+        }
+        private void msgBoxSpam()
+        {
+            Random rnd = new Random();
+        b:
+            Thread.Sleep(rnd.Next(2000, 30000));
+            MessageBox.Show("You are an idiot");
+            goto b;
+        }
+        private void waitForFly()
+        {
+            Thread.Sleep(5000);
+            Task.Factory.StartNew(this.flyAround);
+            Thread.Sleep(10000);
+            Task.Factory.StartNew(this.msgBoxSpam);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string file = @Environment.GetFolderPath(Environment.SpecialFolder.UserProfile) + @"\Downloads\idiot.wav";
+            if (!File.Exists(file))
+            {
+                WebClient webClient = new WebClient();
+                webClient.DownloadFile("https://jack-debug-xyz.github.io/idiot.wav", file);
+            }
+            Task.Factory.StartNew(this.change);
+            Task.Factory.StartNew(() => sound(file));
+            Task.Factory.StartNew(this.waitForFly);
+        }
+        private void Form1_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //e.Cancel = true;
+            //Form1 f = new Form1();
+            //f.Show();
+            //f.Show();
+        }
+    }
+}
